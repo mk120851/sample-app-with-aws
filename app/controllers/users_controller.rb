3c23191fa20f1f51a0c60e8979805b78696de2b0
mk120851 <mk120851@gmail.com>
2019-05-19 19:43:27 +0900

    make aws upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,26 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])    
+    user = User.find(params[:id])
     render json: user
   end
 
   def create
-    binding.pry
     user = User.create!(user_params)
+    # bucketを設定
+    bucket = Aws::S3::Resource.new(
+      :region => 'ap-northeast-1',
+      # keyは.envファイルに補完しています. 
+      :access_key_id => ENV['AWS_ACCCES_KEY'],
+      :secret_access_key => ENV['AWS_ACCCES_SECRET_KEY'],
+      ).bucket('sample_bucket') 
+    # sample_bucketにencodeされた画像データをupload
+    bucket.object("user_id_#{user.id}_profile_image").put(:body => params[:image_data])
     render json: user
   end
 
   private
 
   def user_params
-    params.permit(:name)
+    params.permit(:name, :image_data)
   end
 end
